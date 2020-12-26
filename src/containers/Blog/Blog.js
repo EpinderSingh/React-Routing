fe79edb4b0d30aad1985a6abfff95408ff4c7885
Epinder <epinder.singh@gmail.com>
2020-12-26 16:04:09 +0800

    init commit

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
+
+import './Blog.css';
+
+class Blog extends Component {
+  render() {
+    return (
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/new-post">New post</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        {/* <Route path="/" exact render={() => <h1>Home</h1>} /> */}
+        <Route path="/" exact component={Posts} />
+        <Route path="/new-post" exact component={NewPost} />
+      </div>
+    );
+  }
+}
+
+export default Blog;
