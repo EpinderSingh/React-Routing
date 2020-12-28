fb1d3015ca7c0fe5b9e9a1ae6250b1ae780dc2f6
Epinder <epinder.singh@gmail.com>
2020-12-28 17:14:44 +0800

    condition redirect & 404 page not found

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 
 import './Blog.css';
 
 class Blog extends Component {
+  state = {
+    auth: false,
+  };
+
   render() {
     return (
       <div className="Blog">
@@ -13,7 +17,7 @@ class Blog extends Component {
           <nav>
             <ul>
               <li>
-                <NavLink to="/" exact>
+                <NavLink to="/posts/" exact>
                   Posts
                 </NavLink>
               </li>
@@ -34,8 +38,13 @@ class Blog extends Component {
         </header>
         {/* <Route path="/" exact render={() => <h1>Home</h1>} /> */}
         <Switch>
-          <Route exact path="/new-post" component={NewPost} />
-          <Route path="/" component={Posts} />
+          {this.state.auth ? (
+            <Route path="/new-post" component={NewPost} />
+          ) : null}
+          <Route path="/posts" component={Posts} />
+          <Route render={() => <h1>Page not found!</h1>} />
+          {/* <Redirect from="/" to="/posts" /> */}
+          {/* <Route path="/" component={Posts} /> */}
         </Switch>
       </div>
     );
